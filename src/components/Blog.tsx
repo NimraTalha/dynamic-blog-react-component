d93da220825fbd39e0a4cc0faa5c61a9bec623cf
Nimra Talha <nimratalha@gmail.com>
2024-12-03 15:49:19 +0500

    update everything

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -11,12 +11,10 @@ interface BlogCardProps {
 export default function BlogCard({ post, isDarkBackground }: BlogCardProps) {
   return (
     <Card
-      className={`p-4 ${
-        isDarkBackground ? "bg-slate-800 text-white" : "bg-white text-slate-800"
-      } rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-500`}
+      className={`p-4 ${isDarkBackground ? "bg-slate-800 text-white" : "bg-white text-slate-800"} rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-500`}
     >
       <Image
-        src={post.imageUrl}
+        src={post.imageUrl} // Ensure the image path is correct
         alt={post.title}
         width={400}
         height={200}
@@ -28,14 +26,13 @@ export default function BlogCard({ post, isDarkBackground }: BlogCardProps) {
         <div className="flex flex-col items-center mt-4">
           <p className="text-xs text-gray-500 mt-2">{post.date}</p>
           
-            <Link href={`/posts/${post.id}`} passHref
-            className={`hover:underline mt-4 block w-full px-6 py-2 ${
-              isDarkBackground ? "bg-emerald-500" : "bg-red-500"
-            } text-white rounded-lg hover:bg-red-600 transition-colors duration-300
-              text-center`}
-          >
-            Read More
-          </Link>
+            <Link href={`/posts/${post.id}`} passHref>
+              <a
+                className={`hover:underline mt-4 block w-full px-6 py-2 ${isDarkBackground ? "bg-emerald-500" : "bg-red-500"} text-white rounded-lg hover:bg-red-600 transition-colors duration-300 text-center`}
+              >
+                Read More
+              </a>
+            </Link>
         </div>
       </CardContent>
     </Card>
